014037dd008f7775c82e6746c36b02ab8772c6d8
michelealiffi <michelealiffi890@gmail.com>
2024-09-13 17:32:33 +0200

    [FIX] fix error App

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,23 @@
-<<<<<<< HEAD
 <script setup>
-// import AppHeader from './components/AppHeader.vue';
+import AppHeader from './components/AppHeader.vue';
 // import AppFooter from './components/AppFooter.vue';
 // //import { library } from '@fortawesome/fontawesome-svg-core'
-=======
-<script>
-import AppHeader from './components/AppHeader.vue';
-//import AppFooter from './components/AppFooter.vue';
-//import { library } from '@fortawesome/fontawesome-svg-core'
->>>>>>> 14-create-navbar
 
 // //import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
 
 // //library.add(faUserSecret)
 
-<<<<<<< HEAD
-// export default {
-//   components: {
-//     AppHeader,
-//     AppFooter
-//   },
-// } 
-=======
 export default {
   components: {
     AppHeader
     //AppFooter
   }
 }
->>>>>>> 14-create-navbar
 </script>
 
 <template>
 <!-- <font-awesome-icon icon="fa-solid fa-user-secret" /> -->
-<!-- <AppHeader></AppHeader> -->
+<<AppHeader></AppHeader>
 <router-view/>
 <!-- <AppFooter></AppFooter> -->
 </template>
