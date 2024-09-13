cc2f88958cd36e1c8a2ed2235f8e2c9259f05ce6
michelealiffi <michelealiffi890@gmail.com>
2024-09-13 17:35:32 +0200

    [FIX] minor problem fix

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script>
 import AppHeader from './components/AppHeader.vue';
 // import AppFooter from './components/AppFooter.vue';
 // //import { library } from '@fortawesome/fontawesome-svg-core'
