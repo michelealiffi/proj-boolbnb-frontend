e6c9dde7c83dc7342bd5fa21e335bdb9443f49ed
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-09-22 09:56:48 +0200

    [FIX] suggestions only show after user mouse in

--- src/components/AppSearchBar.vue
+++ src/components/AppSearchBar.vue
@@ -15,7 +15,7 @@ export default {
         return {
             store,
             autocompleteResuluts: [],
-            showAutocompleteList: true,
+            showAutocompleteList: false,
 
             // autocomplete
             autocompletedQuery: "",
